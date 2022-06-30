1246dda7a13fdf059a554f1823e7fb7c65d20243
sevdagulbaran <74216005+sevdagulbaran@users.noreply.github.com>
2022-06-30 22:27:01 +0300

    Shape Bootcamp

---
+++ SwiftUIThinkingBootcamp/Components/ShapesBootcamp.swift
@@ -0,0 +1,30 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Sevda Gul Baran on 30.06.2022.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+        Circle()
+        Rectangle()
+        Ellipse()
+            .fill(Color.orange)
+            .foregroundColor(Color.pink)
+            .stroke()
+            .stroke(Color.red)
+            .stroke(Color.blue, lineWidth: 30)
+            .stroke(Color.orange, style: StrokeStyle(lineWidth: 20, lineCap: .butt, dash: [10]))
+            //the specified area
+            .trim(from: 0.2, to: 1.0)
+    }
+}
+
+struct ShapesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapesBootcamp()
+    }
+}
